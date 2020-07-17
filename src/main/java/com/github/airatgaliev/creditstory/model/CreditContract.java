012f7da2dcdb255b4d0fca1d9abcf592e567f521
Airat Galiev <airat.galiev@gmail.com>
2020-07-17 11:08:45 +0300

    implemented CreditContract and CreditStory models

--- src/main/java/com/github/airatgaliev/creditstory/model/CreditContract.java
+++ src/main/java/com/github/airatgaliev/creditstory/model/CreditContract.java
@@ -6,4 +6,34 @@ public class CreditContract {
 
   private List<Character> paymentDiscipline;
   private CreditStory creditStory;
+
+  public CreditContract(List<Character> paymentDiscipline,
+      CreditStory creditStory) {
+    this.paymentDiscipline = paymentDiscipline;
+    this.creditStory = creditStory;
+  }
+
+  public List<Character> getPaymentDiscipline() {
+    return paymentDiscipline;
+  }
+
+  public void setPaymentDiscipline(List<Character> paymentDiscipline) {
+    this.paymentDiscipline = paymentDiscipline;
+  }
+
+  public CreditStory getCreditStory() {
+    return creditStory;
+  }
+
+  public void setCreditStory(CreditStory creditStory) {
+    this.creditStory = creditStory;
+  }
+
+  @Override
+  public String toString() {
+    return "CreditContract{" +
+        "paymentDiscipline=" + paymentDiscipline +
+        ", creditStory=" + creditStory.getValue() +
+        '}';
+  }
 }
--- src/main/java/com/github/airatgaliev/creditstory/model/CreditStory.java
+++ src/main/java/com/github/airatgaliev/creditstory/model/CreditStory.java
@@ -1,5 +1,18 @@
 package com.github.airatgaliev.creditstory.model;
 
 public enum CreditStory {
+  NEGATIVE("Отрицательная"), DOUBTFUL("Сомнительная"), POSITIVE("Положительная");
+  private String value;
 
+  CreditStory(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
 }
