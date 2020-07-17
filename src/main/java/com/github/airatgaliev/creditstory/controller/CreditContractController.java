a8402e05fb9c01ca33a8531e6c8850a8246fd234
Airat Galiev <airat.galiev@gmail.com>
2020-07-17 19:52:25 +0300

    fixed CreditContractController

--- src/main/java/com/github/airatgaliev/creditstory/controller/CreditContractController.java
+++ src/main/java/com/github/airatgaliev/creditstory/controller/CreditContractController.java
@@ -1,18 +1,21 @@
 package com.github.airatgaliev.creditstory.controller;
 
+import com.github.airatgaliev.creditstory.exceptions.InvalidCharacterException;
 import com.github.airatgaliev.creditstory.model.CreditContract;
 import com.github.airatgaliev.creditstory.model.CreditStory;
 import com.github.airatgaliev.creditstory.repository.ICreditContractRepository;
 import com.github.airatgaliev.creditstory.service.CreditStoryInterpreterService;
+import com.github.airatgaliev.creditstory.service.ValidationService;
 import com.github.airatgaliev.creditstory.view.ICreditContractView;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CreditContractController {
 
   private final ICreditContractRepository creditContractRepository;
   private final ICreditContractView creditContractView;
   private final CreditStoryInterpreterService creditStoryInterpreterService = new CreditStoryInterpreterService();
+  private final ValidationService validationService = new ValidationService();
 
   public CreditContractController(
       ICreditContractRepository creditContractRepository,
@@ -39,12 +42,18 @@ public class CreditContractController {
   }
 
   private List<Character> getPaymentDiscipline() {
-    List<Character> paymentDiscipline = new ArrayList<>();
-    String paymentDisciplineString = creditContractView.getCreditContractPaymentDiscipline();
-    for (char paymentChar : paymentDisciplineString.toCharArray()) {
-      paymentDiscipline.add(paymentChar);
+    while (true) {
+      try {
+        List<Character> paymentDiscipline;
+        String paymentDisciplineString = creditContractView.getCreditContractPaymentDiscipline();
+        validationService.validateStringValue(paymentDisciplineString);
+        paymentDiscipline = paymentDisciplineString.chars().mapToObj(e -> (char) e)
+            .collect(Collectors.toList());
+        return paymentDiscipline;
+      } catch (InvalidCharacterException e) {
+        creditContractView.printExceptionResponse(e);
+      }
     }
-    return paymentDiscipline;
   }
 
   public boolean isRestartedInterpreter() {
