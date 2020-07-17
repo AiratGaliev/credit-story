0f9ad4eccd1b6cbb210a6041433e41c48396dab2
Airat Galiev <airat.galiev@gmail.com>
2020-07-17 10:31:18 +0300

    start project

---
+++ src/main/java/com/github/airatgaliev/creditstory/CreditStoryApp.java
@@ -0,0 +1,11 @@
+package com.github.airatgaliev.creditstory;
+
+/**
+ * Hello world!
+ */
+public class CreditStoryApp {
+
+  public static void main(String[] args) {
+
+  }
+}
---
+++ src/test/java/com/github/airatgaliev/creditstory/CreditStoryAppTest.java
@@ -0,0 +1,19 @@
+package com.github.airatgaliev.creditstory;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CreditStoryAppTest {
+
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
