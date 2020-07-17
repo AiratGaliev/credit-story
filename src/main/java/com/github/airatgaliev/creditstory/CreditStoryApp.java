e68aaa2f021e105e1f8916f59b21d64038967d1b
Airat Galiev <airat.galiev@gmail.com>
2020-07-17 13:07:03 +0300

    edited CreditStoryApp

--- src/main/java/com/github/airatgaliev/creditstory/CreditStoryApp.java
+++ src/main/java/com/github/airatgaliev/creditstory/CreditStoryApp.java
@@ -1,11 +1,23 @@
 package com.github.airatgaliev.creditstory;
 
-/**
- * Hello world!
- */
+import com.github.airatgaliev.creditstory.controller.CreditContractController;
+import com.github.airatgaliev.creditstory.repository.ICreditContractRepository;
+import com.github.airatgaliev.creditstory.repository.InMemoryCreditContractRepositoryImpl;
+import com.github.airatgaliev.creditstory.view.ConsoleCreditContractView;
+import com.github.airatgaliev.creditstory.view.ICreditContractView;
+
 public class CreditStoryApp {
 
   public static void main(String[] args) {
-
+    boolean isRestarted = true;
+    while (isRestarted) {
+      ICreditContractRepository creditContractRepository = new InMemoryCreditContractRepositoryImpl();
+      ICreditContractView creditContractView = new ConsoleCreditContractView();
+      CreditContractController creditContractController = new CreditContractController(
+          creditContractRepository, creditContractView);
+      creditContractController.getAllCreditContracts();
+      creditContractController.createCreditContract();
+      isRestarted = creditContractController.isRestartedInterpreter();
+    }
   }
 }
